75d05e5bedfb7ddfa898f1b7e8ef7341dde78e02
tmikhaylovskaya <tmikhaylovskaya1981@gmail.com>
2025-03-05 00:17:29 +0300

    добавление Lazy инициализации для profile в user

--- src/main/java/com/dmdev/entity/User.java
+++ src/main/java/com/dmdev/entity/User.java
@@ -56,6 +56,6 @@ public class User {
     @JoinColumn(name = "company_id") // company_id
     private Company company;
 
-    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = false)
     private Profile profile;
 }
