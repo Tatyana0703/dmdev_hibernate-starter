df7081c51206d44958d0f0b83055bae9c5e92ffb
tmikhaylovskaya <tmikhaylovskaya1981@gmail.com>
2025-03-18 17:40:40 +0300

    обновление версий

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -17,16 +17,16 @@ public class HibernateRunner {
             session.beginTransaction();
             session.enableFetchProfile("withCompanyAndPayment");
 
-//            var user = session.get(User.class, 1L);
-//            System.out.println(user.getPayments().size());
-//            System.out.println(user.getCompany().getName());
+            var user = session.get(User.class, 1L);
+            System.out.println(user.getPayments().size());
+            System.out.println(user.getCompany().getName());
 
-            var users = session.createQuery(
-                    "select u from User u " +
-                            "where 1 = 1", User.class)
-                    .list();
-            users.forEach(user -> System.out.println(user.getPayments().size()));
-            users.forEach(user -> System.out.println(user.getCompany().getName()));
+//            var users = session.createQuery(
+//                    "select u from User u " +
+//                            "where 1 = 1", User.class)
+//                    .list();
+//            users.forEach(user -> System.out.println(user.getPayments().size()));
+//            users.forEach(user -> System.out.println(user.getCompany().getName()));
 
             session.getTransaction().commit();
         }
