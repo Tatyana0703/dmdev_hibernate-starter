6a5568b350d607377a435d59890b2ca259d7ca01
tmikhaylovskaya <tmikhaylovskaya1981@gmail.com>
2025-03-19 14:33:18 +0300

    обновление версий

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -1,10 +1,10 @@
 package com.dmdev;
 
+import com.dmdev.entity.Payment;
 import com.dmdev.util.HibernateUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-
 import javax.transaction.Transactional;
 import java.sql.SQLException;
 
@@ -17,17 +17,17 @@ public class HibernateRunner {
              Session session = sessionFactory.openSession()) {
             session.doWork(connection -> System.out.println(connection.getTransactionIsolation()));
 
-//            try {
-//                var transaction = session.beginTransaction();
-//
-//                var payment1 = session.find(Payment.class, 1L);
-//                var payment2 = session.find(Payment.class, 2L);
-//
-//                session.getTransaction().commit();
-//            } catch (Exception exception) {
-//                session.getTransaction().rollback();
-//                throw exception;
-//            }
+            try {
+                var transaction = session.beginTransaction();
+
+                var payment1 = session.find(Payment.class, 1L);
+                var payment2 = session.find(Payment.class, 2L);
+
+                session.getTransaction().commit();
+            } catch (Exception exception) {
+                session.getTransaction().rollback();
+                throw exception;
+            }
 //            session.save()
         }
     }
